c76ea7c2ed2064a1bbbbb950f03e6508ac38cf30
Lautaro Frias <lfrias@alu.frlp.utn.edu.ar>
2025-09-06 04:54:44 -0300

    feat(Usuarios): se agrego el authController con metodos login y register

---
+++ pasa_la_pagina/src/main/java/com/example/pasa_la_pagina/controllers/AuthController.java
@@ -0,0 +1,39 @@
+package com.example.pasa_la_pagina.controllers;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.pasa_la_pagina.DTOs.requests.LoginRequest;
+import com.example.pasa_la_pagina.DTOs.requests.RegisterRequest;
+import com.example.pasa_la_pagina.services.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Map<String, String>> register(@RequestBody RegisterRequest request){
+        String token = authService.register(request);
+        return ResponseEntity.ok(Map.of("token",token));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequest request) {
+
+        String token = authService.login(request);
+        return ResponseEntity.ok(Map.of("token",token));
+    }
+    
+    //todo endpoint for login with google
+
+}
