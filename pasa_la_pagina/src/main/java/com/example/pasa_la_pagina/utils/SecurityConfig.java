3ac3d24c2bb1cb5e0859d509a8dba9529e41c13c
Lautaro Frias <lfrias@alu.frlp.utn.edu.ar>
2025-09-06 07:15:08 -0300

    feat(Usuarios): Se agrego el filtro de jwt y se modifico el security Config

--- pasa_la_pagina/src/main/java/com/example/pasa_la_pagina/utils/SecurityConfig.java
+++ pasa_la_pagina/src/main/java/com/example/pasa_la_pagina/utils/SecurityConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,6 +16,8 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JWTFilter jwtFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -25,8 +29,11 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                                 .requestMatchers("/auth/**").permitAll()    //todo agregar endpoints protegidos
-                                .anyRequest().permitAll()
-                );
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
