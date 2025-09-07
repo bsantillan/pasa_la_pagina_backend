a40563a9898ecf5e5e2236a0ffc234060854386a
Lautaro Frias <lfrias@alu.frlp.utn.edu.ar>
2025-09-07 08:29:54 -0300

    Se agrego la logica de usuario service, tanto como para recuperar o modificar

--- pasa_la_pagina/src/main/java/com/example/pasa_la_pagina/controllers/UsuarioController.java
+++ pasa_la_pagina/src/main/java/com/example/pasa_la_pagina/controllers/UsuarioController.java
@@ -3,14 +3,22 @@ package com.example.pasa_la_pagina.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.pasa_la_pagina.DTOs.requests.UpdateUsuarioRequest;
 import com.example.pasa_la_pagina.DTOs.response.RecuperarUsuarioResponse;
 import com.example.pasa_la_pagina.services.UsuarioService;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 @RestController
@@ -24,5 +32,25 @@ public class UsuarioController {
     public ResponseEntity<RecuperarUsuarioResponse> recuperarUsuarioById(@PathVariable Long id) {
         return ResponseEntity.ok(usuarioService.recuperarUsuario(id));
     }
+
+    @GetMapping("/")
+    public ResponseEntity<List<RecuperarUsuarioResponse>> recurarUsuarios() {
+        return ResponseEntity.ok(usuarioService.recuperarTodosUsuarios());
+    }
+
+    @GetMapping("/email")
+    public ResponseEntity<RecuperarUsuarioResponse> recuperarUsuarioByEmail(@RequestParam String email) {
+        return ResponseEntity.ok(usuarioService.recuperarUsuarioByEmail(email));
+    }
+    
+    @PutMapping("update/{id}")
+    public ResponseEntity<RecuperarUsuarioResponse> actualizarUsuario(
+            @PathVariable Long id,
+            @RequestBody UpdateUsuarioRequest request) {
+
+        RecuperarUsuarioResponse updated = usuarioService.actualizarUsuario(id, request);
+        return ResponseEntity.ok(updated);
+    }
+    
     
 }
--- pasa_la_pagina/src/main/java/com/example/pasa_la_pagina/services/UsuarioService.java
+++ pasa_la_pagina/src/main/java/com/example/pasa_la_pagina/services/UsuarioService.java
@@ -1,7 +1,11 @@
 package com.example.pasa_la_pagina.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.example.pasa_la_pagina.DTOs.requests.UpdateUsuarioRequest;
 import com.example.pasa_la_pagina.DTOs.response.RecuperarUsuarioResponse;
 import com.example.pasa_la_pagina.entities.Usuario;
 import com.example.pasa_la_pagina.repositories.UsuarioRepository;
@@ -14,13 +18,42 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
 
-    public RecuperarUsuarioResponse recuperarUsuario(Long id){
-        Usuario usuario = usuarioRepository.findById(id).orElseThrow();
+    private RecuperarUsuarioResponse mapToResponse(Usuario usuario) {
+
         RecuperarUsuarioResponse response = new RecuperarUsuarioResponse();
         response.setId(usuario.getId());
         response.setNombre(usuario.getNombre());
         response.setApellido(usuario.getApellido());
         response.setEmail(usuario.getEmail());
         return response;
+
     }
+
+    public RecuperarUsuarioResponse recuperarUsuario(Long id){
+        Usuario usuario = usuarioRepository.findById(id).orElseThrow();
+        return mapToResponse(usuario);
+    }
+
+    public List<RecuperarUsuarioResponse> recuperarTodosUsuarios(){
+        List<RecuperarUsuarioResponse> listado = new ArrayList<>();
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        for (Usuario usuario : usuarios) {
+            listado.add(mapToResponse(usuario)); 
+        }
+        return listado;
+    }
+
+    public RecuperarUsuarioResponse recuperarUsuarioByEmail(String email){
+        Usuario usuario = usuarioRepository.findByEmail(email).orElseThrow();
+        return mapToResponse(usuario);
+    }
+
+    public RecuperarUsuarioResponse actualizarUsuario(Long id, UpdateUsuarioRequest usuarioRequest){
+        Usuario usuario = usuarioRepository.findById(id).orElseThrow();
+        usuario.setNombre(usuarioRequest.getNombre());
+        usuario.setApellido(usuarioRequest.getApellido());
+        usuarioRepository.save(usuario);
+        return mapToResponse(usuario);
+    }
+
 }
