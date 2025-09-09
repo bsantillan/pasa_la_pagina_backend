b901c7995843b865849f943bbab766c6cf4dc239
bsantillan <brunosaantillan@gmail.com>
2025-09-09 14:25:43 -0300

    feat: Se agrego la clase Autor

---
+++ pasa_la_pagina/src/main/java/com/example/pasa_la_pagina/entities/Autor.java
@@ -0,0 +1,20 @@
+package com.example.pasa_la_pagina.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Autor")
+@Getter 
+@Setter 
+@NoArgsConstructor 
+@AllArgsConstructor 
+@Builder
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre; 
+}
