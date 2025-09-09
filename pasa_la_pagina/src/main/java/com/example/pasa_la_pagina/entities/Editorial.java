1a343e146b319c9de011b0b2c5bf8bda37699bbe
bsantillan <brunosaantillan@gmail.com>
2025-09-09 14:22:18 -0300

    feat: Se agrego la clase Editorial

---
+++ pasa_la_pagina/src/main/java/com/example/pasa_la_pagina/entities/Editorial.java
@@ -0,0 +1,19 @@
+package com.example.pasa_la_pagina.entities;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Editorial")
+@Getter 
+@Setter 
+@NoArgsConstructor 
+@AllArgsConstructor 
+@Builder
+public class Editorial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+}
