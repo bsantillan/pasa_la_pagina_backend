969545860f394b13e8872194cea19a0ddca541c9
bsantillan <brunosaantillan@gmail.com>
2025-09-09 14:27:56 -0300

    feat: Se agrego la clase Foto

---
+++ pasa_la_pagina/src/main/java/com/example/pasa_la_pagina/entities/Foto.java
@@ -0,0 +1,20 @@
+package com.example.pasa_la_pagina.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Foto")
+@Getter 
+@Setter 
+@NoArgsConstructor 
+@AllArgsConstructor 
+@Builder
+public class Foto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "url", nullable = false, length = 100)
+    private String url; 
+}
