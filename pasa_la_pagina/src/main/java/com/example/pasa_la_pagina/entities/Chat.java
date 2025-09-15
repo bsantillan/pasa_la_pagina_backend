af374d2620a973332074b37cb3a4ae1cd9eb24c3
Iñaki Zelayeta <zelayetai9@gmail.com>
2025-09-14 23:45:45 -0300

    feat(Chat): se creo la clase Chat

---
+++ pasa_la_pagina/src/main/java/com/example/pasa_la_pagina/entities/Chat.java
@@ -0,0 +1,23 @@
+package com.example.pasa_la_pagina.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Chat")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor @Builder
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+    @Column(length = 100)
+    private String titulo;
+
+    // Lado inverso
+    // @OneToOne(mappedBy = "chat", fetch = FetchType.LAZY)
+    // private Intercambio intercambio;
+}
