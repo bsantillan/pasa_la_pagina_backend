9d2f2caddf72094af3d5c251ebba13c43793b707
Iñaki Zelayeta <zelayetai9@gmail.com>
2025-09-15 23:12:56 -0300

    chore(SecurityConfig.java): dejar públicas las rutas de swagger

--- pasa_la_pagina/src/main/java/com/example/pasa_la_pagina/utils/SecurityConfig.java
+++ pasa_la_pagina/src/main/java/com/example/pasa_la_pagina/utils/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/auth/**").permitAll()
                     .requestMatchers("/ws/**").permitAll()
+                    .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                     .anyRequest().authenticated()
             )
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
